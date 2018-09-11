8f14026971373bcf91b41c642da89733c115dfcc
pablodiegoss <pablodiegoss@hotmail.com>
2018-09-11 14:08:07 -0300

    Create logic for presets like Hiro inside django view

--- ARte/pwa/views.py
+++ ARte/pwa/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 
 def index(request):
     ctx = {"markers":[
-                {"patt":"peixe", "image":"peixe"},
+                {"patt":"peixe", "image":"alien"},
+                {"preset":"hiro", "image":"alien"},
                 ]
             }
     return render(request, 'pwa/exhibit.jinja2', ctx)
