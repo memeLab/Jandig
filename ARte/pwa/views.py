892d6192388b8e3f40d1739c1f25e4c200c67085
rodrigocam <rodrigo.redcode@gmail.com>
2018-10-03 21:13:44 -0300

    Fix style

--- ARte/pwa/views.py
+++ ARte/pwa/views.py
@@ -5,10 +5,12 @@ def service_worker(request):
     return render(request, 'pwa/sw.js',
                   content_type='application/x-javascript')
 
+
 def index(request):
-    ctx = {"markers":[
-                {"patt":"peixe", "image":"alien"},
-                {"preset":"hiro", "image":"alien"},
-                ]
-            }
+    ctx = {
+        "markers": [
+            {"patt": "peixe", "image": "alien"},
+            {"preset": "hiro", "image": "alien"},
+        ]
+    }
     return render(request, 'pwa/exhibit.jinja2', ctx)
