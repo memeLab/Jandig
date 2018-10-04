6ba91c44042dae1092cb1f7a093e2609e231ce0f
rodrigocam <rodrigo.redcode@gmail.com>
2018-10-04 13:42:29 -0300

    Add workbox to cached files

--- ARte/pwa/jinja2/pwa/sw.js
+++ ARte/pwa/jinja2/pwa/sw.js
@@ -18,6 +18,11 @@ if (workbox) {
     workbox.strategies.networkFirst()
   );
 
+  workbox.routing.registerRoute(
+    'https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js',
+    workbox.strategies.networkFirst()
+  );
+
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
