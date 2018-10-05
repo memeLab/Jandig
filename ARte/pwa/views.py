c1cd2f6c8c855ab9a0474830e394654a60b1f40e
pablodiegoss <pablodiegoss@hotmail.com>
2018-10-05 19:50:48 -0300

    Create Marker Model

--- ARte/pwa/views.py
+++ ARte/pwa/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Marker
 
 def service_worker(request):
     return render(request, 'pwa/sw.js',
@@ -8,25 +8,28 @@ def service_worker(request):
 
 def index(request):
     ctx = {"markers":[
-                {"preset":"hiro", "image":"none"},
-                {"patt":"peixe", "image":"peixe"}, 
-                {"patt":"andando", "image":"andando"}, 
-                {"patt":"robo-pula", "image":"robo-pula"},
-                {"patt":"robo-rodas", "image":"robo-rodas"}, 
-                {"patt":"robo3dandando", "image":"robo3dandando"}, 
-                {"patt":"robo3dvoando", "image":"robo3dvoando"}, 
-                {"patt":"saucer", "image":"saucer"}, 
-                {"patt":"temaki", "image":"temaki"}, 
-                # {"patt":"antipodas", "image":"antipodas"}, # Blinking
+                Marker(patt="gueixa", gif="gueixa"),
+                Marker(patt="temaki", gif="temaki"),
+                Marker(patt="robo-rodas", gif="robo-rodas", scale="1 1.5"),
+                Marker(patt="tokusatsu", gif="tokusatsu"),
+                Marker(patt="samurai", gif="samurai", scale="1.5 1.5"),
+                Marker(patt="antipodas", gif="antipodas"), # Blinking
+
+                # Marker(patt="hiro", gif="none"),
+                # {"patt":"peixe", "image":"peixe"},
+                # {"patt":"andando", "image":"andando"},
+                # {"patt":"robo-pula", "image":"robo-pula"},
+                # {"patt":"robo3dandando", "image":"robo3dandando"},
+                # {"patt":"robo3dvoando", "image":"robo3dvoando"},
+                # {"patt":"saucer", "image":"saucer"},
+                # {"patt":"flyingsaucer", "image":"andando"},
+                # {"patt":"manekineko", "image":"robo-rodas"},
+
                 # {"patt":"janela", "image":"janela"}, # Gif bugging
-                    # {"patt":"manekineko", "image":"manekineko"}, Not Working
-                    # {"patt":"alien", "image":"alien"}, Not Working
                     # {"patt":"binoculos", "image":"janela"}, Not Working
-                    # {"patt":"flyingsaucer", "image":"flyingsaucer"}, Not Working
-                    # {"patt":"gueixa", "image":"gueixa"}, Not Working 
                     # {"patt":"gueixa2", "image":"gueixa2"}, Not Working
                     # {"patt":"iemanja", "image":"iemanja"}, Not Working
-                    # {"patt":"pedinhazinha", "image":"pedrinhazinha"}, Not Working
+                    # {"patt":"pedrinhazinha", "image":"pedrinhazinha"}, Not Working
                 ]
             }
     return render(request, 'pwa/exhibit.jinja2', ctx)
