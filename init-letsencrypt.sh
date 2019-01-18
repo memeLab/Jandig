6fa64c78c82b525b4a7102a57e8ab68626b5f23b
pablodiegoss <pablodiegoss@hotmail.com>
2019-01-18 17:22:14 -0200

    Update ports

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-domains=(arte.memelab.com.br www.arte.memelab.com.br)
+domains=(jandig2.memelab.com.br www.jandig2.memelab.com.br)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
