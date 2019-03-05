320483aaca36be2beb79f90bf3c905886509bae1
pablodiegoss <pablodiegoss@hotmail.com>
2019-03-05 13:11:18 -0300

    Change multi-marker folder location

--- ARte/pwa/static/js/multi-marker.js
+++ ARte/pwa/static/js/multi-marker.js
@@ -1,9 +1,5 @@
 function createMarkerFile() {
-    // create absoluteBaseURL
-    var link = document.createElement('a')
-    link.href = ARjs.Context.baseURL
-    var absoluteBaseURL = link.href
-
+    var absoluteBaseURL = window.location
     // create the base file
     var file = {
         meta: {
@@ -21,21 +17,19 @@ function createMarkerFile() {
             poseMatrix: new THREE.Matrix4().makeTranslation(0, 0, 0).toArray(),
         }
     }
-    if (trackingBackend === 'artoolkit') {
-        file.subMarkersControls[0].parameters.type = 'pattern'
-        file.subMarkersControls[0].parameters.patternUrl = absoluteBaseURL + 'patts/pattern-letterC.patt'
-        file.subMarkersControls[1].parameters.type = 'pattern'
-        file.subMarkersControls[1].parameters.patternUrl = absoluteBaseURL + 'patts/pattern-kanji.patt'
-        file.subMarkersControls[2].parameters.type = 'pattern'
-        file.subMarkersControls[2].parameters.patternUrl = absoluteBaseURL + 'patts/pattern-letterF.patt'
-        file.subMarkersControls[3].parameters.type = 'pattern'
-        file.subMarkersControls[3].parameters.patternUrl = absoluteBaseURL + 'patts/pattern-hiro.patt'
-    } else console.assert(false)
+    file.subMarkersControls[0].parameters.type = 'pattern'
+    file.subMarkersControls[0].parameters.patternUrl = absoluteBaseURL + 'static/patts/pattern-letterC.patt'
+    file.subMarkersControls[1].parameters.type = 'pattern'
+    file.subMarkersControls[1].parameters.patternUrl = absoluteBaseURL + 'static/patts/pattern-kanji.patt'
+    file.subMarkersControls[2].parameters.type = 'pattern'
+    file.subMarkersControls[2].parameters.patternUrl = absoluteBaseURL + 'static/patts/pattern-letterF.patt'
+    //file.subMarkersControls[3].parameters.type = 'pattern'
+    //file.subMarkersControls[3].parameters.patternUrl = absoluteBaseURL + 'static/patts/pattern-hiro.patt'
+    
 
     // json.strinfy the value and store it in localStorage
     return file
 }
 
 const file = createMarkerFile();
-console.table(file.subMarkersControls)
 localStorage.setItem('ARjsMultiMarkerFile', JSON.stringify(file))
