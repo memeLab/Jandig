97a7e92769272bbcf536aef880ff5ef636cecec4
rodrigocam <rodrigo.redcode@gmail.com>
2019-03-05 19:49:53 -0300

    Remove collect and unused file

--- src/ARte/collect/js/fetch-worker.js
+++
@@ -1,8 +0,0 @@
-   
-/* Web worker for download the gif in background */
-
-self.addEventListener('message', function(e) {
-    const buff = fetch('/' + e.data)
-                    .then(resp => resp.arrayBuffer())
-                    .then(buff => self.postMessage(buff))
-})
--- src/ARte/collect/js/main.js
+++
@@ -1,6 +0,0 @@
-if ('serviceWorker' in navigator) {
-  // Use the window load event to keep the page load performant
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js');
-  });
-}
