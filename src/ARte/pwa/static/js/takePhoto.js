d4e0a107b7819e0335bfb86ac122422f4ea8357f
pablodiegoss <pablodiegoss@hotmail.com>
2019-03-06 00:19:27 -0300

    Update function to allow picture download

--- src/ARte/pwa/static/js/takePhoto.js
+++ src/ARte/pwa/static/js/takePhoto.js
@@ -1,14 +1,27 @@
 document.getElementsByClassName("screenshot-button")[0].addEventListener("click", function () {
     console.log("AAAAA")
     const scene = document.getElementsByTagName("canvas")[0]
-    
-    const img = document.createElement('img');
-    scene.toBlob(function (blob) {
-        console.log(blob)
-        img.src = URL.createObjectURL(blob);
-        console.log(img)
-        // saveAs(blob, "Dashboard.png"); 
-    });
-    console.log(scene.toDataURL("image/png"))
+    const photoDataURI = scene.toDataURL("image/png")
 
+    var link = document.createElement('a');
+
+    link.download = getPhotoFileName() + '.png';
+    link.href = photoDataURI;
+    link.click();
 })
+
+function getPhotoFileName() {
+    const date = new Date();
+    const day = formatSmallNumber(date.getDate());
+    const month = formatSmallNumber(date.getMonth() + 1); // Months are counted from 0-11 here.
+    const year = date.getFullYear();
+    const hour = formatSmallNumber(date.getHours());
+    const minutes = formatSmallNumber(date.getMinutes());
+    const seconds = formatSmallNumber(date.getSeconds());
+    const milli = date.getMilliseconds();
+    return year + month + day + "_" + hour + minutes + seconds + milli
+}
+
+function formatSmallNumber(number) {
+    return (number < 10) ? "0" + number : number
+}
