bf0bbcf66ca081fc7b9d952014cc01c94c356369
rodrigocam <rodrigo.redcode@gmail.com>
2019-03-28 22:17:24 -0300

    Add javascript alert when an iOS device access the app through a browser other than Safari

--- src/ARte/pwa/static/js/detect.js
+++ src/ARte/pwa/static/js/detect.js
@@ -8,4 +8,13 @@ const isIos = () => {
 
 if (isIos()) {
     document.getElementById("manifest").href = "static/ios-manifest.json";
+    const browser = bowser.getParser(window.navigator.userAgent)
+    const isValidBrowser = browser.satisfies({
+      safari: ">=9"
+    })
+    if(!isValidBrowser) {
+        alert(
+          "Desculpe, este aplicativo funciona apenas no navegador Safari :("
+        )
+    }
 }
