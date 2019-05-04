6235d81c6167f40a04e90e2074b41c827e9028f5
rodrigocam <rodrigo.redcode@gmail.com>
2019-05-04 14:07:53 -0300

    Adjust flex items and add ar viwer

--- src/ARte/core/urls.py
+++ src/ARte/core/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from .views import service_worker, index, upload_image
-from .views_s.home import home
+from .views_s.home import home, ar_viewer
 
 
 urlpatterns = [
+    path('', home, name='home'),
+    path('ar', ar_viewer, name='ar_viewer'),
+    # path('community', *, *),
+    # path('docs', *, *),
+    # path('agreement', *, *),
+    # path('agreement', *, *),
+
     path('sw.js', service_worker, name='sw'),
     path('upload', upload_image, name='upload-image'),
-    path('', home, name='home'),
 ]
--- src/ARte/core/views_s/home.py
+++ src/ARte/core/views_s/home.py
@@ -4,3 +4,7 @@ from django.urls import reverse
 
 def home(request):
     return render(request, 'core/home.jinja2')
+
+
+def ar_viewer(request):
+    return render(request, 'core/exhibit.jinja2')
