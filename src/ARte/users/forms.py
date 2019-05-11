57334325ba888a4348bde8e40629c1e48f03a2d2
rodrigocam <rodrigo.redcode@gmail.com>
2019-05-11 18:22:42 -0300

    Adjust signup form css

--- src/ARte/users/forms.py
+++ src/ARte/users/forms.py
@@ -8,21 +8,31 @@ class SignupForm(UserCreationForm):
     """
     Form to register a new user
     """
-    first_name = forms.CharField(
-        max_length=30,
-        required=True,
-        help_text='Your first name'
-    )
-    last_name = forms.CharField(
-        max_length=30,
-        required=True,
-        help_text='Your last name'
-    )
+
+    # first_name = forms.CharField(
+    #     max_length=30,
+    #     required=True,
+    #     help_text='Your first name',
+
+    # )
+    # last_name = forms.CharField(
+    #     max_length=30,
+    #     required=True,
+    #     help_text='Your last name'
+    # )
     email = forms.EmailField(
         max_length=254,
-        help_text='Your e-mail address'
+        help_text='Your e-mail address',
     )
+
+    def __init__(self, *args, **kwargs):
+        super(SignupForm, self).__init__(*args, **kwargs)
+        
+        self.fields['email'].widget.attrs['placeholder'] = 'email'
+        self.fields['username'].widget.attrs['placeholder'] = 'chosen username'
+        self.fields['password1'].widget.attrs['placeholder'] = 'password'
+        self.fields['password2'].widget.attrs['placeholder'] = 'confirm password'
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+        fields = ['email', 'username', 'password1', 'password2']
