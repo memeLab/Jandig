b5d9b2e2213dd0b37235893d0a95a56f7b8bd119
pablodiegoss <pablodiegoss@hotmail.com>
2019-05-25 19:53:11 -0300

    Try to make urls work

--- src/ARte/config/urls.py
+++ src/ARte/config/urls.py
@@ -4,10 +4,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('core.urls')),
     path('users/', include('users.urls')),
-
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    
+    path('', include('core.urls')),
 ]
 
 if settings.DEBUG:
--- src/ARte/core/urls.py
+++ src/ARte/core/urls.py
@@ -1,16 +1,16 @@
-from django.urls import path
-from .views import service_worker, index, upload_image
+from django.urls import path, re_path
+from .views import service_worker, index, upload_image, exhibit_select, exhibit
 from .views_s.home import home, ar_viewer, community, docs
 
-
 urlpatterns = [
-    path('', home, name='home'),
-    path('ar', ar_viewer, name='ar_viewer'),
-    path('community', community, name='community'),
-    path('docs', docs, name='docs'),
+    re_path('^$', home, name='home'),
+    #path('ar', ar_viewer, name='ar_viewer'),
+    re_path('^community$', community, name='community'),
+    re_path('^docs$', docs, name='docs'),
+    re_path('^exhibit_select$', exhibit_select, name='exhibit_select'),
     # path('agreement', *, *),
     # path('agreement', *, *),
-
-    path('sw.js', service_worker, name='sw'),
-    path('upload', upload_image, name='upload-image'),
+    re_path('sw.js', service_worker, name='sw'),
+    re_path('upload', upload_image, name='upload-image'),
+    re_path('.*', exhibit, name="exhibits"),
 ]
