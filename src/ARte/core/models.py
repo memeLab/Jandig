4eb1ecfb599cb69c77e4b2e540044faa6710f80f
pablodiegoss <pablodiegoss@hotmail.com>
2019-06-16 15:43:38 -0300

    Add Exhibit properties for creation date and artworks_count

--- src/ARte/core/models.py
+++ src/ARte/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from users.models import Artwork, Profile
+from datetime import datetime
 import urllib
 
 class Artwork2(models.Model):
@@ -15,6 +16,15 @@ class Exhibit(models.Model):
     name = models.CharField(unique=True, max_length=50)
     slug = models.CharField(unique=True, max_length=50)
     artworks = models.ManyToManyField(Artwork,related_name="exhibits")
+    creation_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+
+    @property
+    def artworks_count(self):
+        return self.artworks.count()
+
+    @property
+    def date(self):
+        return self.creation_date.strftime("%d/%m/%Y")
