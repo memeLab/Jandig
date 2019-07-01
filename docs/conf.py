09decbc419f080b0f0189c7e86b5f85f01882e47
rodrigocam <rodrigo.redcode@gmail.com>
2019-07-01 00:18:04 -0300

    Add reset css to docs

--- docs/conf.py
+++ docs/conf.py
@@ -57,3 +57,6 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+def setup(app):
+    app.add_css_file('reset.css')
