adcb47ea21403015f49e4b8d60c85aed2cf8f9d1
Pablo Diego <pablodiegoss@hotmail.com>
2020-01-23 00:56:08 -0300

    Create helpers to save objects.all() on user session

--- src/ARte/core/helpers.py
+++ src/ARte/core/helpers.py
@@ -1,4 +1,5 @@
 from django.contrib.staticfiles.templatetags.staticfiles import static
+from users.views import Artwork, Exhibit, Object, Marker
 
 
 def handle_upload_image(image):
@@ -6,3 +7,19 @@ def handle_upload_image(image):
     with open(path, 'wb+') as destination:
         for chunk in image.chunks():
             destination.write(chunk)
+
+
+def get_stuff(model_name, model):
+    def function(request):
+        try:
+            session_stuff = request.session[model_name]
+        except(KeyError):
+            request.session[model_name] = model.objects.all()
+            session_stuff = request.session[model_name]
+        return session_stuff
+    return function
+
+get_artworks = get_stuff('artworks', Artwork)
+get_exhibits = get_stuff('exhibits', Exhibit)
+get_markers = get_stuff('markers', Marker)
+get_objects = get_stuff('objects', Object)
