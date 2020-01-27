c65a532a48ce298cf4d334e821e9204056dc136b
sharkaroid <anacforcelli@usp.br>
2020-01-27 20:23:39 -0300

    fixed django admin page

--- src/ARte/config/urls.py
+++ src/ARte/config/urls.py
@@ -4,10 +4,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('', include('core.routes')),
     path('users/', include('users.urls')),
-    path('admin/', admin.site.urls),
     path('docs/', include('docs.urls'), name='docs'),
 ]
 
