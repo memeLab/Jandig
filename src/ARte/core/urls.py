d4a613df77b8cd0bcde4a34750d80964cdaa2cd5
sharkaroid <anacforcelli@usp.br>
2020-02-07 12:36:34 -0300

    fixed typo

--- src/ARte/core/urls.py
+++ src/ARte/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path, include
-from .views import service_worker, index, upload_image, exhibit_select, collection, exhibit_detail, artwork_preview,
+from .views import service_worker, index, upload_image, exhibit_select, collection, exhibit_detail, artwork_preview
 from .views_s.home import home, ar_viewer, community, marker_generator, documentation
 
 urlpatterns = [
