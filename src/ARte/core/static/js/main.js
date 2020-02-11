a59f3becc07596bc6a23134d00598626807f2711
Lucca Eppinger <luccaepp@gmail.com>
2020-02-11 17:07:40 -0300

    Rotation lock in app, in the browser is reloaded when change the orientation.

--- src/ARte/core/static/js/main.js
+++ src/ARte/core/static/js/main.js
@@ -3,6 +3,7 @@ if ('serviceWorker' in navigator) {
   // Use the window load event to keep the page load performant
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js');
-    console.log("AUSDHAUSDHADSUHDASUHUDASH")
+    console.log("AUSDHAUSDHADSUHDASUHUDASH");
+
   });
 }
