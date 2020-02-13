bc729c36f7d114c48dc5a33874ebf5c0ee61f69f
Lucca Eppinger <luccaepp@gmail.com>
2020-02-13 14:49:42 -0300

    Small fixes

--- src/ARte/core/static/js/main.js
+++ src/ARte/core/static/js/main.js
@@ -3,7 +3,5 @@ if ('serviceWorker' in navigator) {
   // Use the window load event to keep the page load performant
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js');
-    console.log("AUSDHAUSDHADSUHDASUHUDASH");
-
   });
 }
