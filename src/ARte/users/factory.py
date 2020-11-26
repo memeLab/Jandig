e617010d7a7eb98c7f14630995e1d1ac16318ff9
MatheusBlanco <msallesblanco@gmail.com>
2020-11-26 10:47:13 -0300

    creating factory file for mock

--- src/ARte/users/factory.py
+++ src/ARte/users/factory.py
@@ -1,9 +1,18 @@
 import factory
 from factory.django import DjangoModelFactory
 
-from django.contrib.auth.models import Object
+from .models import Object
+from django.contrib.auth.models import User
+
+class UserFactory(DjangoModelFactory):
+    username = 'Testador'
+    email = 'test2019ador@gmail.com'
+
+    class Meta:
+        model = User
 
 class ObjectFactory(DjangoModelFactory):
+    id = 1
     owner = 'Matheus'
     source = 'objects/osaka.gif'
     uploaded_at = (2020, 11, 25, 14, 30, 0)
--- src/ARte/users/tests.py
+++ src/ARte/users/tests.py
@@ -3,27 +3,14 @@ import factory
 from django.test import TestCase, Client, RequestFactory
 from unittest import mock
 
+from .factory import ObjectFactory, UserFactory
 from .views import edit_object
-from .factory import ObjectFactory
 
 class EditObjectTestCase(TestCase):
     def setUp(self):
         self.client_test = RequestFactory()
 
-    def test_redirect_to_edit_object_page(self):
-        # request = self.client_test.get('/objects/edit/', follow=True)
-        # response = edit_object(request)
-        # self.assertEqual(response.status_code, 200)
-        pass
-
-    def test_edit_object_title(self):
-        pass
-
-    def test_edit_object_image(self):
-        pass
-
-    def test_edit_object_scale(self):
-        pass
-
-    def test_edit_object_position(self):
-        pass
+    def test_redirect(self):
+        request = self.client_test.get('objects/edit', follow=True)
+        response = edit_object(request)
+        self.assertEqual(response.status_code, 200)
