2bf9537b6189547f351739eb14a41e8ba0739f9c
Gabriel Davi <gdavi.gd@gmail.com>
2021-03-24 23:44:39 -0300

    371 - add view mode to the profile page
    Co-authored-by: Welison Almeida <welison.almeida.923@gmail.com>
    Co-authored-by: marcos-mv <vinicius_kof36@hotmail.com>

--- src/ARte/users/views.py
+++ src/ARte/users/views.py
@@ -14,6 +14,8 @@ from django.utils.translation import ugettext_lazy as _
 from django.http import Http404
 from django.http import HttpResponse
 from django.views.decorators.cache import cache_page
+from django.utils.functional import SimpleLazyObject
+
 
 
 from .forms import SignupForm, RecoverPasswordCodeForm, RecoverPasswordForm, UploadMarkerForm, UploadObjectForm, ArtworkForm, ExhibitForm, ProfileForm, PasswordChangeForm
@@ -124,7 +126,14 @@ def invalid_recovering_email_or_username(request):
 
 @login_required
 def profile(request):
-    profile = Profile.objects.select_related().get(user=request.user)
+   
+    user = request.GET.get('user')
+    
+    if user:
+        profile = Profile.objects.get(user=user)
+    else:
+         profile = Profile.objects.select_related().get(user=request.user)
+
 
     exhibits = profile.exhibits.all()
     markers = profile.marker_set.all()
@@ -136,7 +145,8 @@ def profile(request):
         'artworks': artworks,
         'markers':markers,
         'objects':objects,
-        'profile':True
+        'profile':True, 
+        'button_enable': False if user else True
     }
     return render(request, 'users/profile.jinja2', ctx)
 
@@ -298,6 +308,7 @@ def element_get(request):
 	    'id_object' : element.augmented.id,
             'type': element_type,
             'author': element.author.user.username,
+            'owner_id': element.author.user.id,
             'exhibits': element.exhibits_count,
             'created_at': element.created_at.strftime('%d %b, %Y'),
             'marker': element.marker.source.url,
@@ -312,6 +323,7 @@ def element_get(request):
             'type': element_type,
             'author': element.author,
             'owner': element.owner.user.username,
+            'owner_id': element.owner_id,
             'artworks': element.artworks_count,
             'exhibits': element.exhibits_count,
             'source': element.source.url,
