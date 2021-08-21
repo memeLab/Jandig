c38865ff8e662fd27e830d63392b3fe0966e0085
Pablo Silva <pablodiegoss@hotmail.com>
2021-08-21 18:29:27 -0300

    Add Sentry

--- src/ARte/config/settings.py
+++ src/ARte/config/settings.py
@@ -36,6 +36,23 @@ ALLOWED_HOSTS += CUSTOM_ALLOWED_HOSTS
 print(f"ALLOWED_HOSTS:{ALLOWED_HOSTS}")
 # Application definition
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://081a2c3476b24a9f9a51d74bde539b62@o968990.ingest.sentry.io/5920229",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
