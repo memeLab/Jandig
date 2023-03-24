362048e3d52f35033c1ca3b14efc042a58b0a920
Rodrigo Oliveira <rodrigo.redcode@gmail.com>
2023-03-23 22:30:13 -0300

    ar viewer working

--- frontend/src/ar_viewer/ArViewer.js
+++ frontend/src/ar_viewer/ArViewer.js
@@ -1,69 +1,55 @@
 import React, { useState, useEffect } from "react";
 
-
 export const ArViewer = (props) => {
-   const [exhibit, setExhibit] = useState({});
-   const [artworks, setArtworks] = useState([]);
    const [markers, setMarkers] = useState([]);
 
-   const getExhibitData = async () => {
-      const exhibit = await fetch(props.exhibitUrl, {
-         headers : { 
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-         }
-       }).then(res => res.json());
-
-      setExhibit(exhibit);
-
-      const artworks = await Promise.all(
-         exhibit.artworks.map(async (id) => {
-            return await fetch(`http://localhost:8000/api/v1/artworks/${id}`, {
-               headers : { 
-                  'Content-Type': 'application/json',
-                  'Accept': 'application/json'
-               }
-            }).then(res => res.json());
-         })
-      );
-      
-      setArtworks(artworks);
-
-      const markers = await Promise.all(
-         artworks.map(async (artwork) => {
-            return [await fetch(`http://localhost:8000/api/v1/markers/${artwork.marker}`, {
-               headers : { 
-                  'Content-Type': 'application/json',
-                  'Accept': 'application/json'
-               }
-            }).then(res => res.json()),
-             await fetch(`http://localhost:8000/api/v1/objects/${artwork.augmented}`, {
-               headers : { 
-                  'Content-Type': 'application/json',
-                  'Accept': 'application/json'
-               }
-            }).then(res => res.json())
-            ]
-         })
-      );
-      
-      setMarkers(markers)
-   }
 
    useEffect(() => {
+      const getExhibitData = async () => {
+         const exhibit = await fetch(props.exhibitUrl, {
+            headers : { 
+               'Content-Type': 'application/json',
+               'Accept': 'application/json'
+            }
+          }).then(res => res.json());
+
+         const artworks = await Promise.all(
+            exhibit.artworks.map(async (id) => {
+               return await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/v1/artworks/${id}`, {
+                  headers : { 
+                     'Content-Type': 'application/json',
+                     'Accept': 'application/json'
+                  }
+               }).then(res => res.json());
+            })
+         );
+         
+         const markers = await Promise.all(
+            artworks.map(async (artwork) => {
+               return [await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/v1/markers/${artwork.marker}`, {
+                  headers : { 
+                     'Content-Type': 'application/json',
+                     'Accept': 'application/json'
+                  }
+               }).then(res => res.json()),
+                await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/v1/objects/${artwork.augmented}`, {
+                  headers : { 
+                     'Content-Type': 'application/json',
+                     'Accept': 'application/json'
+                  }
+               }).then(res => res.json())
+               ]
+            })
+         );
+         
+         setMarkers(markers)
+      }
       getExhibitData();
-   }, []);
+  }, [props.exhibitUrl]);
 
    return (
       <div>
-         <ar-scene>
-      {markers && markers.map(([marker, content], i) => (
-         <ar-marker patt={marker.patt} key={marker.id}>
-            <ar-content src={content.source}></ar-content>
-         </ar-marker>
-         )
-      )}
-         </ar-scene>
+      {markers.length > 0 && <ar-scene>{markers && markers.map(([marker, content], i) => (<ar-marker patt={marker.patt} key={marker.id}><ar-content src={content.source} scale={content.scale} position={content.position} rotation={content.rotation}></ar-content></ar-marker>))}</ar-scene>}
       </div>
    );
 }
