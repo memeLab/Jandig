a4abd1f6367047ae69a1bea101402e8b9bb7b92f
Pablo Silva <pablodiegoss@hotmail.com>
2024-05-26 18:21:28 +0100

    Update ProfileAdmin

--- src/users/admin.py
+++ src/users/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 
 from users.models import Profile
 
-admin.site.register(Profile)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display=["id", "username", "email", "created"]
+    ordering=["-id"]
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related("user")
+    
+    def username(self, obj):
+        return obj.user.username
+    
+    def email(self,obj):
+        return obj.user.email
+    
+    def created(self,obj):
+        return obj.user.date_joined
