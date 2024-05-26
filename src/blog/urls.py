c721e43f1b337f361e7add1c3123d61cf1fe626b
Pablo Silva <pablodiegoss@hotmail.com>
2024-05-26 18:23:04 +0100

    Add blog URLs

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,3 +1,10 @@
 # blog/urls.py
 
 from django.urls import path
+from blog import views
+
+urlpatterns = [
+    path("", views.blog_index, name="blog_index"),
+    path("post/<int:pk>/", views.blog_detail, name="blog_detail"),
+    path("category/<category>/", views.blog_category, name="blog_category"),
+]
