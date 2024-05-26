e29f5c60bd9b8362330ddfdf5fc8a681d89ee72b
Pablo Silva <pablodiegoss@hotmail.com>
2024-05-26 18:22:51 +0100

    Create views for Blog index, categories and post details

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,2 +1,37 @@
 from django.shortcuts import render
 
+from blog.models import Post
+
+PREVIEW_SIZE=300
+def blog_index(request):
+
+    posts = Post.objects.all().order_by("-created")
+    context = {
+        "posts": posts,
+        "PREVIEW_SIZE": PREVIEW_SIZE,
+    }
+    return render(request, "blog/index.jinja2", context)
+
+def blog_category(request, category):
+
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by("-created")
+
+    context = {
+        "category": category,
+        "posts": posts,
+        "PREVIEW_SIZE": PREVIEW_SIZE,
+    }
+
+    return render(request, "blog/category.jinja2", context)
+
+def blog_detail(request, pk):
+
+    post = Post.objects.get(pk=pk)
+
+    context = {
+        "post": post,
+    }
+
+    return render(request, "blog/detail.jinja2", context)
