c23d5ecd2e10b83d66d45ca30387bc1d0402102b
Pablo Silva <pablodiegoss@hotmail.com>
2024-05-26 18:21:49 +0100

    Add admin for Posts and Categories

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,2 +1,12 @@
 from django.contrib import admin
 
+from blog.models import Post, Category
+
+admin.site.register(Category)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    raw_id_fields=("author",)
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related("author")
