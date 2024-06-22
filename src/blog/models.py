78b4b1fe05feb99ecf287cad8ad54d56ba8e539c
Pablo Silva <pablodiegoss@hotmail.com>
2024-06-22 16:14:26 +0100

    Update blog model fields

--- src/blog/models.py
+++ src/blog/models.py
@@ -7,22 +7,20 @@ class PostStatus(models.TextChoices):
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-
     def __str__(self):
         return self.name
-
     class Meta:
         verbose_name_plural = "categories"
 
 class Post(models.Model):
+    id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, unique=True)
     status = models.CharField(
         choices=PostStatus.choices, max_length=20, default=PostStatus.DRAFT
     )
-    author = models.ForeignKey(Profile, on_delete=models.DO_NOTHING, related_name="posts")
+    author = models.ForeignKey(Profile, on_delete=models.DO_NOTHING, related_name="posts", null=True, blank=True)
     body = models.TextField()
-    created = models.DateTimeField(auto_now_add=True, editable=True)
+    created = models.DateTimeField(editable=True)
     updated = models.DateTimeField(auto_now=True, editable=True)
     categories = models.ManyToManyField(Category, related_name='posts', null=True, blank=True)
 
