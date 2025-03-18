9160655528eb699f6afbfaab2761098b7b12cb38
Pablo Silva <pablodiegoss@hotmail.com>
2025-03-18 19:59:42 +0000

    Disable work box and service worker

--- src/core/jinja2/core/sw.js
+++ src/core/jinja2/core/sw.js
@@ -1,32 +1,32 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
 
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
+// if (workbox) {
+//   console.log(`Yay! Workbox is loaded 🎉`);
 
-  workbox.routing.registerRoute(
-    new RegExp('.*'),
-    workbox.strategies.networkFirst()
-  );
-  workbox.routing.registerRoute(
-    new RegExp('.*\.js'),
-    workbox.strategies.networkFirst()
-  );
+//   workbox.routing.registerRoute(
+//     new RegExp('.*'),
+//     workbox.strategies.networkFirst()
+//   );
+//   workbox.routing.registerRoute(
+//     new RegExp('.*\.js'),
+//     workbox.strategies.networkFirst()
+//   );
 
-  workbox.routing.registerRoute(
-    '/',
-    workbox.strategies.networkFirst()
-  );
+//   workbox.routing.registerRoute(
+//     '/',
+//     workbox.strategies.networkFirst()
+//   );
 
-  workbox.routing.registerRoute(
-    /.*\.(?:png|jpg|jpeg|svg|gif|patt)/,
-    workbox.strategies.networkFirst()
-  );
+//   workbox.routing.registerRoute(
+//     /.*\.(?:png|jpg|jpeg|svg|gif|patt)/,
+//     workbox.strategies.networkFirst()
+//   );
 
-  workbox.routing.registerRoute(
-    'https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js',
-    workbox.strategies.networkFirst()
-  );
+//   workbox.routing.registerRoute(
+//     'https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js',
+//     workbox.strategies.networkFirst()
+//   );
 
-} else {
-  console.log(`Boo! Workbox didn't load 😬`);
-}
+// } else {
+//   console.log(`Boo! Workbox didn't load 😬`);
+// }
