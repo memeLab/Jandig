e28209c51c14dcd4f399075ffac4c185ecf6b85e
Pablo Silva <pablodiegoss@hotmail.com>
2025-04-06 22:41:39 +0100

    Fix admin preview for videos

--- src/core/admin.py
+++ src/core/admin.py
@@ -9,6 +9,7 @@ from django.utils.html import format_html
 admin.site.register(Exhibit)
 admin.site.register(Artwork)
 
+
 def create_link_to_related_artworks(obj):
     """Link to related Artworks"""
     if obj.artworks_count == 0:
@@ -19,6 +20,7 @@ def create_link_to_related_artworks(obj):
 
     link = reverse("admin:index") + "core/artwork/?id__in=" + str(artworks_list)
     return format_html('<a href="{}">{}</a>', link, obj._artworks_count)
+
 
 @admin.register(Marker)
 class MarkerAdmin(admin.ModelAdmin):
@@ -65,18 +67,18 @@ class MarkerAdmin(admin.ModelAdmin):
         if obj.file_size > 0:
             return f"{obj.file_size / 1024 / 1024:.2f} MB"
         return obj.file_size
-    
 
     filesize.short_description = "File Size"
     filesize.admin_order_field = "file_size"
     artworks_count.admin_order_field = "_artworks_count"
     exhibits_count.admin_order_field = "_exhibits_count"
 
+
 @admin.register(Object)
 class ObjectAdmin(admin.ModelAdmin):
     list_display = [
         "title",
-        "image_preview",
+        "preview",
         "id",
         "owner",
         "author",
@@ -107,16 +109,26 @@ class ObjectAdmin(admin.ModelAdmin):
     def exhibits_count(self, obj):
         return obj._exhibits_count
 
-    def image_preview(self, obj):
+    def preview(self, obj):
         """Image preview with proportions"""
         max_height = 200
         max_width = 200
         height = max_height * obj.yproportion
         width = max_width * obj.xproportion
+        if obj.is_video:
+            return format_html(
+                '<video width="{}" height="{}" controls><source src="{}" type="video/mp4"></video>',
+                width,
+                height,
+                obj.source.url,
+            )
         return format_html(
-            '<img src="{}" style="height:{}px; width:{}px;"/>', obj.source.url, height, width
+            '<img src="{}" style="height:{}px; width:{}px;"/>',
+            obj.source.url,
+            height,
+            width,
         )
-    
+
     def filesize(self, obj):
         """File size in MB"""
         if obj.file_size > 0:
