e48263140ae58d88c9cf26dde7c8b25eaa1d92f9
Pablo Silva <pablodiegoss@hotmail.com>
2025-04-06 23:04:32 +0100

    Reduce duplications on new admin pages

--- src/core/admin.py
+++ src/core/admin.py
@@ -22,8 +22,7 @@ def create_link_to_related_artworks(obj):
     return format_html('<a href="{}">{}</a>', link, obj._artworks_count)
 
 
-@admin.register(Marker)
-class MarkerAdmin(admin.ModelAdmin):
+class BaseMarkerObjectAdmin(admin.ModelAdmin):
     list_display = [
         "title",
         "image_preview",
@@ -57,11 +56,6 @@ class MarkerAdmin(admin.ModelAdmin):
     def exhibits_count(self, obj):
         return obj._exhibits_count
 
-    def image_preview(self, obj):
-        return format_html(
-            '<img src="{}" style="height:200px; width:200px;"/>', obj.source.url
-        )
-
     def filesize(self, obj):
         """File size in MB"""
         if obj.file_size > 0:
@@ -74,42 +68,17 @@ class MarkerAdmin(admin.ModelAdmin):
     exhibits_count.admin_order_field = "_exhibits_count"
 
 
+@admin.register(Marker)
+class MarkerAdmin(BaseMarkerObjectAdmin):
+    def image_preview(self, obj):
+        return format_html(
+            '<img src="{}" style="height:200px; width:200px;"/>', obj.source.url
+        )
+
+
 @admin.register(Object)
-class ObjectAdmin(admin.ModelAdmin):
-    list_display = [
-        "title",
-        "preview",
-        "id",
-        "owner",
-        "author",
-        "artworks_count",
-        "exhibits_count",
-        "filesize",
-        "uploaded_at",
-    ]
-    search_fields = ["title", "id"]
-    ordering = ["-uploaded_at"]
-
-    def get_queryset(self, request):
-        queryset = (
-            super()
-            .get_queryset(request)
-            .select_related("owner")
-            .prefetch_related("artworks", "artworks__exhibits")
-        )
-        queryset = queryset.annotate(
-            _artworks_count=Count("artworks", distinct=True),
-            _exhibits_count=Count("artworks__exhibits", distinct=True),
-        )
-        return queryset
-
-    def artworks_count(self, obj):
-        return create_link_to_related_artworks(obj)
-
-    def exhibits_count(self, obj):
-        return obj._exhibits_count
-
-    def preview(self, obj):
+class ObjectAdmin(BaseMarkerObjectAdmin):
+    def image_preview(self, obj):
         """Image preview with proportions"""
         max_height = 200
         max_width = 200
@@ -128,14 +97,3 @@ class ObjectAdmin(admin.ModelAdmin):
             height,
             width,
         )
-
-    def filesize(self, obj):
-        """File size in MB"""
-        if obj.file_size > 0:
-            return f"{obj.file_size / 1024 / 1024:.2f} MB"
-        return obj.file_size
-
-    filesize.short_description = "File Size"
-    filesize.admin_order_field = "file_size"
-    # artworks_count.admin_order_field = "_artworks_count"
-    # exhibits_count.admin_order_field = "_exhibits_count"
