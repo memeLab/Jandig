69654b1fe1d4100a4c0f7d5c249cb986cba9dbee
Pablo Silva <pablodiegoss@hotmail.com>
2025-04-10 19:45:42 +0100

    Use SQLite to run tests for faster CI

--- src/config/test_settings.py
+++ src/config/test_settings.py
@@ -12,3 +12,11 @@ STORAGES = {
 
 # Set MEDIA_URL to include the test server URL
 MEDIA_URL = "http://testserver"
+
+# Set up SQLite in-memory database for testing
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
