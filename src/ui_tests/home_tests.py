17a774f154a89ebc5542008bc96c738d48a89ceb
Pablo Silva <pablodiegoss@hotmail.com>
2025-04-21 21:14:11 +0100

    Fix UI test

--- src/ui_tests/home_tests.py
+++ src/ui_tests/home_tests.py
@@ -10,13 +10,13 @@ def test_home_loads_in_english(page: Page):
 def test_changing_language_to_portuguese_and_back_to_english(page: Page) -> None:
     page.goto("http://localhost:8000/")
     page.locator(".trigger-lang-modal").click()
-    page.get_by_label("pt-br").check()
+    page.get_by_label("Brazilian Portuguese").check()
     page.get_by_role("button", name="Ok").click()
     expect(page).to_have_url("http://localhost:8000/")
 
     expect(page.locator("#main_title")).to_contain_text("Bem vindo ao Jandig")
     page.locator(".trigger-lang-modal").click()
-    page.get_by_label("en-us").check()
+    page.get_by_label("Inglês").check()
     page.get_by_role("button", name="Ok").click()
     expect(page).to_have_url("http://localhost:8000/")
     expect(page.locator("#main_title")).to_contain_text("Welcome to Jandig")
