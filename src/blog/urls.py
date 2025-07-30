ef7c33c2ff05b9799657a07045110b1c4c1c5c62
Pablo Silva <pablodiegoss@hotmail.com>
2025-07-30 19:03:20 +0100

    Refactor blog index view

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -6,7 +6,7 @@ from blog import views
 
 urlpatterns = [
     path("", views.blog_index, name="blog_index"),
-    path("post/<int:pk>/", views.blog_detail, name="blog_detail"),
+    path("post/<int:pk>/", views.post_detail, name="post_detail"),
     path("clipping/", views.clipping, name="clipping"),
     path("category/<category>/", views.blog_category, name="blog_category"),
 ]
