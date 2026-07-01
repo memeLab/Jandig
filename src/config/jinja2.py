11e3cd2fc32258c3158ee28fc122106978391a22
Pablo Silva <pablodiegoss@hotmail.com>
2026-07-01 20:42:58 +0100

    Format numbers on modal pages

--- src/config/jinja2.py
+++ src/config/jinja2.py
@@ -5,6 +5,22 @@ from django.utils import translation
 from jinja2 import Environment
 
 
+def filesizeformat(value):
+    """Format a file size in bytes to a human-readable string."""
+    try:
+        size = float(value)
+    except (TypeError, ValueError):
+        return "0 B"
+    if size < 1024:
+        return f"{int(size)} B"
+    elif size < 1024 * 1024:
+        return f"{size / 1024:.1f} KB"
+    elif size < 1024 * 1024 * 1024:
+        return f"{size / (1024 * 1024):.1f} MB"
+    else:
+        return f"{size / (1024 * 1024 * 1024):.1f} GB"
+
+
 def environment(**options):
     options["extensions"] = ["jinja2.ext.i18n"]
     env = Environment(**options)
@@ -21,6 +37,8 @@ def environment(**options):
             ],
         }
     )
+
+    env.filters["filesizeformat"] = filesizeformat
 
     env.install_gettext_translations(translation, newstyle=True)
     return env
